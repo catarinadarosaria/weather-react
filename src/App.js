3c3541d512431e46e8ee54d78fd5e76935ab6b32
Catarina da Rosária <catarinadarosaria@gmail.com>
2023-11-08 18:03:30 +0100

    transfered weather project to react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
-      <Weather city="Porto" />
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,119 @@
 import React from "react";
-import axios from "axios";
+import "./Weather.css";
 
-export default function Weather(props) {
-  function handleresponse(response) {
-    alert(
-      ` Hello from ${props.city}, it is ${Math.round(
-        response.data.main.temp
-      )}ºC`
-    );
-  }
+export default function Weather() {
+  return (
+    <div className="container">
+      <div className="weather-app">
+        <form id="search-form" className="mb-3 search-bar">
+          <div className="row">
+            <div className="col-8">
+              <input
+                type="search"
+                placeholder="Type a city..."
+                className="form-control city-bar"
+                id="city-input"
+                autoComplete="off"
+              />
+            </div>
+            <div className="col-2">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-primary w-100 search-button"
+                id="search-location-button"
+              />
+            </div>
+            <div className="col-2">
+              <button
+                className="btn btn-primary w-100 search-button"
+                id="current-location-button"
+              >
+                current
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="weather-info">
+          <div className="header">
+            <div className="row">
+              <div className="col-4 text-left">
+                <h1>Lille</h1>
+                <p className="date-time">Sunday, 17:13</p>
+                <h3>Clouds</h3>
+                <ul className="precise-weather">
+                  <li id="humidity">Humidity: 20%</li>
+                  <li id="wind">Wind: 20km/h</li>
+                </ul>
+              </div>
+              <div className="col-4 current-weather text-center">
+                <img src="images/sun.png" alt="Icon - Sun" id="icon" />
+              </div>
+              <div className="col-4 current-temperature text-right">
+                <div>
+                  <span className="temperature" id="temperature-value">
+                    13
+                  </span>
+                  <span className="units">
+                    <a href="/" id="celsius">
+                      ºC
+                    </a>{" "}
+                    |
+                    <a href="/" id="fahrenheit">
+                      ºF
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
 
-  let apiKey = "e60cfe91a731d94cdd654022271b22a3";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(handleresponse);
-
-  return <h1>Hi there!</h1>;
+          <div className="week-weather row">
+            <div className="col-2">
+              <div className="week-data week-day">Mon</div>
+              <img src="images/sun.png" alt="Sun" />
+              <div className="week-data daily-temperature">
+                <strong>9º</strong>/2º
+              </div>
+            </div>
+            <div className="col-2">
+              <div className="week-data week-day">Tue</div>
+              <img src="images/sun-cloud.png" alt="Sun with Clouds" />
+              <div className="week-data daily-temperature">
+                <strong>10º</strong>/0º
+              </div>
+            </div>
+            <div className="col-2">
+              <div className="week-data week-day">Wed</div>
+              <img src="images/sun-cloud.png" alt="Sun with Clouds" />
+              <div className="week-data daily-temperature">
+                <strong>17º</strong>/6º
+              </div>
+            </div>
+            <div className="col-2">
+              <div className="week-data week-day">Thu</div>
+              <img src="images/cloud.png" alt="Clouds" />
+              <div className="week-data daily-temperature">
+                <strong>15º</strong>/11º
+              </div>
+            </div>
+            <div className="col-2">
+              <div className="week-data week-day">Fri</div>
+              <img src="images/rain.png" alt="Rain" />
+              <div className="week-data daily-temperature">
+                <strong>12º</strong>/10º
+              </div>
+            </div>
+            <div className="col-2">
+              <div className="week-data week-day">Sat</div>
+              <img src="images/sun.png" alt="Sun" />
+              <div className="week-data daily-temperature">
+                <strong>12º</strong>/8º
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
